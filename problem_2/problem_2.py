1ff7de911ab81d3751a2c5cab1908db1269a9de9
Keith Petersen <krp@keithrpetersen.com>
2019-01-05 22:10:21 -0600

    use itertools.cycle to simplify

--- problem_2/problem_2.py
+++ problem_2/problem_2.py
@@ -1,52 +1,28 @@
 import os
+from itertools import cycle
 
-def get_freqs(data, freqs):
-        for x, item in enumerate(data):
-            freqs.append(freqs[x] + data[x])
-            print(freqs[x])
+def get_data():
+    home_dir = os.path.expanduser('~')
+    input_path = f'{home_dir}/code/advent-of-code/problem_2/input.txt'
+    data = []
+    with open(input_path, 'r') as f:
+        for line in f:
+            data.append(int(line))
 
-        return freqs
+    return data
 
-def get_freqs2(data, freqs):
-        for d in data:
-            for f in freqs:
-                freqs.append(f + d)
-                print(f)
+def find_repeated_frequency(data):
+    freqs = set()
+    current_freq = 0
+    for change in cycle(data):
+        freqs.add(current_freq)
+        current_freq = current_freq + change
 
-        return freqs
+        if current_freq in freqs:
+            return current_freq
 
-def find_duplicates(data):
-    seen = {}
-    dupes = []
+    return None
 
-    for x in data:
-        if x not in seen:
-            seen[x] = 1
-        else:
-            if seen[x] == 1:
-                dupes.append(x)
-            seen[x] += 1
-    
-    return dupes
-
-def freq_counter():
-    data = [3, 3, 4, -2, -4]
-
-    freqs = [0]
-    dupes = []
-    
-    #while len(dupes) == 0:
-    freqs = get_freqs(data, freqs)
-    dupes = find_duplicates(freqs)
-    #if len(dupes) == 0:
-    #    freqs = freqs[-1:]
-    print('f', freqs)
-    print('d', dupes)
-
-    freqs = get_freqs(data, freqs[-1:])
-    dupes = find_duplicates(freqs)
-    print('f', freqs)
-    print('d', dupes)
-        
 if __name__ == "__main__":
-    freq_counter()
+    data = get_data()
+    print(find_repeated_frequency(data))
