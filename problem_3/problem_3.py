c71c50abca249dcdf498209c9c8ad95268a18d0a
Keith Petersen <krp@keithrpetersen.com>
2019-01-08 20:40:46 -0600

    correct letter counting

--- problem_3/problem_3.py
+++ problem_3/problem_3.py
@@ -9,25 +9,22 @@ def calc_checksum(test_data):
     twos = 0
     threes = 0
     for box in test_data:
-        print(box)
         letter_count = {}
         for letter in box:
             if letter in letter_count:
                 letter_count[letter] += 1
             else:
                 letter_count[letter] = 1
-            
-            if 2 in letter_count.values():
-                twos += 1
-            if 3 in letter_count.values():
-                threes += 1
-            print(letter_count)
-    
-    print(twos, threes)
+        
+        if 2 in letter_count.values():
+            twos += 1
+        if 3 in letter_count.values():
+            threes += 1
+
     return twos * threes
 
 
 if __name__ == "__main__":
-    #print(get_data())
+    data = get_data()
     test_data = ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab']
-    print(calc_checksum(test_data))
+    print(calc_checksum(data))
