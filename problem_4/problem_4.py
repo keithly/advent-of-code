52527549fd4895b645effb08a7199fb2ded7c339
Keith Petersen <krp@keithrpetersen.com>
2019-01-10 12:27:56 -0600

    successful but pretty brute-force approach

--- problem_4/problem_4.py
+++ problem_4/problem_4.py
@@ -5,8 +5,19 @@ def get_data():
     return [line.strip() for line in input_path.open()]
 
 
+def find_common_letters(data):
+    for box_1 in data:
+        for box_2 in data:
+            common_letters = ''
+            for letter_1, letter_2 in zip(box_1, box_2):
+                if letter_1 == letter_2:
+                    common_letters += letter_1
+            if len(common_letters) == len(box_1) - 1:
+                print(box_1)
+                print(box_2)
+                return common_letters
+
 
 if __name__ == "__main__":
-    #data = get_data()
     test_data = ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']
-
+    print(find_common_letters(get_data()))
